cebfab4cc967558c4a9510a7b31362a15344f986
LeticiaFig <leticia22f@yahoo.com.br>
2020-09-07 18:28:28 -0300

    Teste

--- bot/views.py
+++ bot/views.py
@@ -35,18 +35,13 @@ def event(requests):
         output = "Senha atual: {0}\nSua vez chegou!".format(senha)
         send_message_onlyText(chat_id, output)
     elif command == "Confirmar":
-        senha = random.randint(100, 200)
-        output = "Siga em frente por 10 metros e depois vire a direita e caminhe por mais 3 metros. Em seguida, à sua direita, estarão as cadeiras e a, à sua esquerda, estará o balcão de atendimento. Sua senha é {0}, a senha atual é {1}. Você receberá mensagens com o andamento da fila.".format(
-            senha, senha - 3)
+        output = "Consulta agendada com sucesso!"
         send_message_onlyText(chat_id, output)
         time.sleep(random.randint(1, 30))
-        output = "Senha atual: {0}\nVocê será atendido em breve".format(senha - 2)
+        output = "Olá! Você possui uma consulta agendada para o dia 15/09 às 11:00."
         send_message_onlyText(chat_id, output)
         time.sleep(random.randint(1, 45))
-        output = "Senha atual: {0}\nVocê é o próximo da fila!".format(senha - 1)
-        send_message_onlyText(chat_id, output)
-        time.sleep(random.randint(1, 60))
-        output = "Senha atual: {0}\nSua vez chegou!".format(senha)
+        output = "Olá! Não se esqueça da sua consulta com o Dr Arthur hoje às 11:00. Quando chegar na clínica, clique no botão abaixo escrito “cheguei”"
         send_message_onlyText(chat_id, output)
 
     else:
